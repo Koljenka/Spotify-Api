9e186d24a5e46c8ca20d3f82994fec11f2725cac
Kolja Nagel <kn685832@fh-muenster.de>
2022-08-13 18:34:00 +0200

    changed image to java

--- generateApiClient.sh
+++ generateApiClient.sh
@@ -2,4 +2,6 @@
 
 rm -r ./gen
 
-npx @openapitools/openapi-generator-cli generate -g typescript-angular -i ./swagger/swagger-full.yaml -c ./openapi-codegen-config.yaml -o ./gen
+wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.0.1/openapi-generator-cli-6.0.1.jar -O openapi-generator-cli.jar
+
+java -jar openapi-generator-cli.jar generate -g typescript-angular -i ./swagger/swagger-full.yaml -c ./openapi-codegen-config.yaml -o ./gen

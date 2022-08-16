495a5eb78829b3e9665a2db7ae669d210c3a8c9d
Kolja Nagel <kn685832@fh-muenster.de>
2022-08-16 09:39:27 +0200

    deactivate auto deploy until angular 14 is supported

--- generateApiClient.sh
+++ generateApiClient.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -r ./gen
+rm -rf ./gen
 
 wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.0.1/openapi-generator-cli-6.0.1.jar -O openapi-generator-cli.jar
 
